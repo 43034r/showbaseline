1dfc3764c89948ed9a884bd0331c8541bac66610
Aleksandr Romanenkov <91538748+43034r@users.noreply.github.com>
2023-08-14 13:03:51 +0300

    default= in os.getenv

--- src/showbaseline.py
+++ src/showbaseline.py
@@ -7,13 +7,13 @@ import requests
 import random
 import threading
 
-DT_API_TOKEN = os.getenv('DT_API_TOKEN', 'dt0c01.X.X')
-DT_API_URL = os.getenv('DT_API_URL', 'https://provide-API-URL-WITH-END/api/v1')
-SVC_LISTSTR = os.getenv( 'SVC_LIST', "SERVICE-F531D98ABDEE9F9D, SERVICE-D2E55F5D48FF41A1")
-LOG_LEVEL:int = os.getenv('LOG_LEVEL', 0) 
-UPDATE_INTERVAL:int = os.getenv('UPDATE_INTERVAL', 5) # in minutes
-A_SEND_MINMAX:int = os.getenv('A_SEND_MINMAX', 0) # send min\max or only median
-A_SEND_COUNT:int = os.getenv('A_SEND_COUNT', 1)  # send count or not 0 - not, 1 - yes.
+DT_API_TOKEN = os.getenv('DT_API_TOKEN', default='dt0c01.X.X')
+DT_API_URL = os.getenv('DT_API_URL', default='https://provide-API-URL-WITH-END/api/v1')
+SVC_LISTSTR = os.getenv( 'SVC_LIST', default="SERVICE-F531D98ABDEE9F9D, SERVICE-D2E55F5D48FF41A1")
+LOG_LEVEL:int = os.getenv('LOG_LEVEL', default=0) 
+UPDATE_INTERVAL:int = os.getenv('UPDATE_INTERVAL', default=5) # in minutes
+A_SEND_MINMAX:int = os.getenv('A_SEND_MINMAX', default=0) # send min\max or only median
+A_SEND_COUNT:int = os.getenv('A_SEND_COUNT', default=1)  # send count or not 0 - not, 1 - yes.
 SVC_LIST = SVC_LISTSTR.split(',')
 
 
